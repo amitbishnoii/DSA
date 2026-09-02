a5da0716d8d875e1aa25be56ae35aad651d6806c
amitbishnoii <amitbishnoi29012@gmail.com>
2026-09-03 00:36:25 +0530

    practice medium hard patterns

--- patterns/pattern.js
+++ patterns/pattern.js
@@ -71,11 +71,11 @@ function Pattern5() {
 }
 
 function Pattern6() {
-// 12345
-// 1234
-// 123
-// 12
-// 1
+    // 12345
+    // 1234
+    // 123
+    // 12
+    // 1
     for (let i = 5; i >= 1; i--) {
         for (let j = 1; j <= i; j++) {
             process.stdout.write(`${j}`);
@@ -85,11 +85,163 @@ function Pattern6() {
 }
 
 function Pattern7() {
+    //     *
+    //    ***
+    //   *****
+    //  *******
+    // *********
     for (let i = 0; i < 5; i++) {
-        for (let j = 0; j < (2*i + 1); j++) {
-            process.stdout.write(`*`);
+        for (let j = 0; j < 5 - i - 1; j++) {
+            process.stdout.write(" ");
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            process.stdout.write("*");
+        }
+        for (let l = 0; l < 5 - i - 1; l++) {
+            process.stdout.write(" ");
         }
         console.log();
     }
 }
-Pattern7()
+
+function Pattern8() {
+    // *********
+    //  *******
+    //   *****
+    //    ***
+    //     *
+    for (let i = 4; i >= 0; i--) {
+        for (let j = 0; j < 5 - i - 1; j++) {
+            process.stdout.write(" ");
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            process.stdout.write("*");
+        }
+        for (let l = 0; l < 5 - i - 1; l++) {
+            process.stdout.write(" ");
+        }
+        console.log();
+    }
+}
+
+function Pattern9() {
+    //     *
+    //    ***
+    //   *****
+    //  *******
+    // *********
+    // *********
+    //  *******
+    //   *****
+    //    ***
+    //     *
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j < 5 - i - 1; j++) {
+            process.stdout.write(" ");
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            process.stdout.write("*");
+        }
+        for (let l = 0; l < 5 - i - 1; l++) {
+            process.stdout.write(" ");
+        }
+        console.log();
+    }
+    for (let i = 4; i >= 0; i--) {
+        for (let j = 0; j < 5 - i - 1; j++) {
+            process.stdout.write(" ");
+        }
+        for (let k = 0; k < 2 * i + 1; k++) {
+            process.stdout.write("*");
+        }
+        for (let l = 0; l < 5 - i - 1; l++) {
+            process.stdout.write(" ");
+        }
+        console.log();
+    }
+}
+
+function Pattern10(n) {
+    // *
+    // **
+    // ***
+    // ****
+    // *****
+    // ****
+    // ***
+    // **
+    // *
+    for (let i = 0; i < 2 * n - 1; i++) {
+        let stars = i;
+        if (i > n) stars = 2 * n - i;
+        for (let j = 1; j <= stars; j++) {
+            process.stdout.write("*");
+        }
+        console.log();
+    }
+}
+
+function Pattern11() {
+    // 1
+    // 01
+    // 101
+    // 0101
+    // 10101
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j <= i; j++) {
+            if (i % 2 === 0) {
+                if (j % 2 === 0) {
+                    process.stdout.write(`${1}`);
+                } else {
+                    process.stdout.write(`${0}`);
+                }
+            } else {
+                if (j % 2 === 0) {
+                    process.stdout.write(`${0}`);
+                } else {
+                    process.stdout.write(`${1}`);
+                }
+            }
+        }
+        console.log();
+    }
+}
+
+function Pattern12() {
+    // 1      1
+    // 12    21
+    // 123  321
+    // 12344321
+    for (let i = 1; i < 5; i++) {
+        for (let j = 1; j <= i; j++) {
+            process.stdout.write(`${j}`);
+        }
+        for (let j = 0; j < 2 * (4 - i); j++) {
+            process.stdout.write(" ");
+        }
+        for (let j = i; j >= 1; j--) {
+            process.stdout.write(`${j}`);
+        }
+        console.log();
+    }
+}
+
+function Pattern13() {
+// 1 
+// 2 3 
+// 4 5 6 
+// 7 8 9 10 
+// 11 12 13 14 15 
+    let a = 1;
+    for (let i = 0; i < 5; i++) {
+        for (let j = 0; j <= i; j++) {
+            process.stdout.write(`${a} `);
+            a += 1;
+        }
+        console.log();
+    }
+}
+
+function Pattern14() {
+    
+}
