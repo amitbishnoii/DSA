50078d166da459786e8017b67521d52e9cdca3c4
amitbishnoii <amitbishnoi29012@gmail.com>
2026-09-09 19:17:45 +0530

    optimize leetcode 977

--- two-pointers/leetcode_977.js
+++ two-pointers/leetcode_977.js
@@ -2,14 +2,17 @@ var sortedSquares = function (nums) {
     if (nums.length === 1) return nums.map((num) => num * num);
     let i = 0;
     let j = nums.length - 1;
-    let result = [];
+    let pos = nums.length - 1;
+    let result = new Array(nums.length);
 
     while (i <= j) {
         if (Math.abs(nums[i]) > Math.abs(nums[j])) {
-            result.unshift(nums[i] * nums[i]);
+            result[pos] = nums[i] * nums[i];
+            pos--;
             i++;
         } else {
-            result.unshift(nums[j] * nums[j]);
+            result[pos] = nums[j] * nums[j];
+            pos--;
             j--;
         }
     }
