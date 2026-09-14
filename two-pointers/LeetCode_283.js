2d1a752598c2089f917606726fe8bfa922ebbe17
amitbishnoii <amitbishnoi29012@gmail.com>
2026-09-14 16:08:04 +0530

    solve leetcode 283

---
+++ two-pointers/LeetCode_283.js
@@ -0,0 +1,22 @@
+/**https://leetcode.com/problems/move-zeroes/
+ * @param {number[]} nums
+ * @return {void} Do not return anything, modify nums in-place instead.
+ */
+var moveZeroes = function (nums) {
+    let i = 0;
+    let j = 0;
+
+    while (i < nums.length) {
+        if (nums[i] === 0) {
+            i++;
+        } else {
+            nums[j] = nums[i];
+            j++;
+            i++;
+        }
+    }
+    while (j < nums.length) {
+        nums[j] = 0;
+        j++;
+    }
+};
